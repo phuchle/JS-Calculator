873c0cb9cf296339f456ea57bbd27cc51ce8d93a
Phuc Le <le.phuc.h@gmail.com>
2017-03-21 17:48:50 -0700

    Add some styling and functions for button presses and displaying what has been entered.

--- index.js
+++ index.js
@@ -4,3 +4,57 @@
 
 // user pushes the numbers and operators to chain an expression
 // the numbers are shown on the display and then the expression is executed when the equal button is pressed
+
+// --- need a way to detect which buttons are pushed
+// --- need a way to track the history of entered numbers and operations
+// --- need to display the cache on #display as user presses buttons
+// delete function
+// equal function
+
+var cache = [];
+
+var detectButtonClick = () => {
+  var buttons = document.querySelectorAll("div#numbers div span, div#operators p");
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      if (button.id === "delete") {
+        popFromCache(); // deletes one character from end of cache
+      } else if (button.id === "clear") {
+        clearCache();
+      } else if (button.id === "equals") {
+        sumCache();
+      } else {
+        storeButtonValue(button);
+      }
+    });
+  });
+}
+
+var storeButtonValue = button => {
+  cache.push(button.textContent);
+  updateDisplay();
+}
+
+var popFromCache = () => {
+  cache.pop();
+  updateDisplay();
+}
+
+var clearCache = () => {
+  cache = [];
+  updateDisplay();
+}
+
+var sumCache = () => {
+
+}
+
+var updateDisplay = () => {
+  var display = document.querySelector("div#display");
+  display.textContent = cache.join("");
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  detectButtonClick();
+});
